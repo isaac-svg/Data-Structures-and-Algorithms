d564dd191fe54bb9e3054c1ed7d70e64cc95d05f
isaac-svg <isaacsakyi009@gmail.com>
2023-03-29 03:33:44 +0000

    DSA

--- index.js
+++ index.js
@@ -278,27 +278,473 @@ function FindPair3(arr, target) {
   }
   return -1;
 }
-const testArr = [1, 2, 3, 4, 5];
+// const testArr = [1, 2, 3, 4, 5];
 // console.log(FindPair3(testArr, 7));
 //
 //  find the longest substring
-const strings = ["abc", "abc", "ab"];
+const strings = [""];
 function findMaxPrefix(arr) {
   let sortedArray = arr.sort();
 
   let cache = new Set();
-
+  if (arr.length == 1) {
+    return arr[0];
+  }
   for (let i = 0; i < arr.length; i++) {
     for (let j = i + 1; j < arr.length; j++) {
       if (sortedArray[0][i] === sortedArray[j][i]) {
-        // prefix += sortedArray[i][i];
-        cache.add(sortedArray[i][i], "hello");
+        cache.add(sortedArray[i][i], "");
       } else {
+        console.log("hello");
         return ` ${Array.from(cache.keys()).join("")}`;
       }
     }
   }
-  return ` ${Array.from(cache.keys()).join("")}`;
+
+  return "";
 }
 
 // console.log(findMaxPrefix(strings));
+
+/**
+ * Find the Pair in two Arrays
+Given two array X and Y. Find a pair of elements (xi, yi) such that xi∈X and yi∈Y where xi+yi=value.
+ */
+
+/**
+ * First approach: Exhaustive search or Brute force, loop through element xi of X and see if you can find
+(value – xi) in Y. Two for loop.
+The Time Complexity is O(n2) and Space Complexity is O(1)
+ */
+console.time("s1");
+
+function findPairInTwo1(X, Y, target) {
+  //  asuumption X and Y are of the same length
+
+  for (let i = 0; i < X.length; i++) {
+    for (let j = 0; j < Y.length; j++) {
+      if (Y[j] === target - X[i]) {
+        return [X[i], Y[j]];
+      }
+    }
+  }
+  return "pair not found";
+}
+console.timeEnd("s1");
+
+// const tesArr1 = [1, 2, 3, 4, 5];
+// const tesArr2 = [6, 7, 8, 9, 10];
+// console.log(findPairInTwo1(tesArr1, tesArr2, 14));
+
+/**
+ * Second approach: Sorting, Sort all the elements in the second array Y. For each element if X you can see
+if that element is there in Y by using binary search.
+Sorting algorithms take O(m. log m) and searching will take O(n. log m) time.
+The Time Complexity of an algorithm is O(n.log m) or O(m.log m) and Space Complexity is O(1)
+ */
+function findPairInTwo2() {}
+/**
+ * Third approach: Sorting, Steps are as follows:
+1. Sort the elements of both X and Y in increasing order.
+2. Take the sum of the smallest element of X and the largest element of Y.
+3. If the sum is equal to value, we got our pair.
+4. If the sum is smaller than value, take next element of X
+5. If the sum is greater than value, take the previous element of Y
+Sorting algorithms take O(n.log n) + O(m.log m) for sorting and searching will take O(n+m) time.
+The Time Complexity of an algorithm is O(n.log n) Space Complexity is O(1)
+ */
+
+function findPairInTwo3(X, Y, target) {}
+
+/**
+ * Forth approach: Hash-Table, Steps are as follows:
+1. Scan through all the elements in the array Y and insert them into Hashtable.
+2. Now scan through all the elements of array X, let us suppose the current element is xi see if you can
+find (value - xi) in the Hashtable.
+3. If you find the value, you got your pair.
+4. If not, then go to the next value in the array X.
+Hash-Table insert and find take constant time O(1) so the total Time Complexity of the algorithm is O(n)
+time. Space Complexity is also O(n)
+ */
+console.time("start");
+function findPairInTwo4(X, Y, target) {
+  let cache = new Map();
+  for (let i = 0; i < X.length; i++) {
+    cache.set(target - X[i], X[i]);
+  }
+  for (let j = 0; j < Y.length; j++) {
+    if (cache.has(Y[j])) {
+      return [cache.get(Y[j]), Y[j]];
+    }
+  }
+}
+console.timeEnd("start");
+
+// const tesArr1 = [1, 2, 3, 4, 5];
+// const tesArr2 = [6, 7, 8, 9, 10];
+// console.log(findPairInTwo4(tesArr1, tesArr2, 14));
+
+/**
+ * Two elements whose sum is closest to zero
+Given an Array of integers, both +ve and -ve. You need to find the two elements such that their sum is
+closest to zero.
+ */
+
+/**
+ * First approach: Exhaustive search or Brute force, for each element in array find the other element whose
+value when added will give minimum absolute value. This is done using two for loop, first loop to select
+the element and second loop to find the element that should be added to it so that the absolute of the sum
+will be minimum or close to zero.
+The Time Complexity is O(n2) and Space Complexity is O(1)
+ */
+console.time("closestSumToZero1");
+function closestSumToZero1(arr) {
+  let minSum, minFirst, minSecond, sum;
+  // Inialization of variables
+  minFirst = 0;
+  minSecond = 1;
+
+  // check if array size is greater than two
+  if (arr.length < 2) {
+    return;
+  }
+
+  minSum = Math.abs(arr[minFirst] + arr[minSecond]);
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      sum = Math.abs(arr[i] + arr[j]);
+      if (sum < minSum) {
+        minFirst = i;
+        minSecond = j;
+        minSum = sum;
+      }
+    }
+  }
+  return [arr[minFirst], arr[minSecond]];
+}
+// const testData = [-1, 2, 4, -3, 9, -7, -1, 2, 4, -3, 9, -7];
+// console.log(closestSumToZero1(testData));
+console.timeEnd("closestSumToZero1");
+
+/**
+ * Second approach: Sorting
+Steps are as follows:
+1. Sort all the elements in the array.
+2. Take two variable firstIndex = 0 and secondIndex = size -1
+3. Compute sum = arr[firstIndex]+arr[secondIndex]
+4. If the sum is equal to the 0 then we have the solution
+5. If the sum is less than the 0 then we will increase first
+6. If the sum is greater than the 0 then we will decrease the second
+7. We repeat the above process 3 to 6, till we get the desired pair or we get first >= second
+ */
+
+function closestSumToZero2(arr) {
+  let minFirst, minSecond, sum, minSum, left, right;
+
+  //  check if array length is leaa than two
+
+  if (arr.length < 2) {
+    return;
+  }
+
+  //  sort the array
+  sort(arr);
+
+  //  inialize variables
+  minFirst = 0;
+  minSecond = arr.length - 1;
+  minSum = Math.abs(arr[minFirst] + arr[minSecond]);
+
+  for (left = 0; left < arr.length; left++) {
+    sum = Math.abs(arr[minFirst] + arr[minSecond]);
+    if (sum < minSum) {
+      minSum = sum;
+      minFirst = left;
+      minSecond = right;
+    }
+    if (sum < 0) {
+      left++;
+    }
+    if (sum > 0) {
+      right--;
+    } else {
+      break;
+    }
+  }
+}
+
+/**
+ * Find maxima in a bitonic array
+A bitonic array comprises of an increasing sequence of integers immediately followed by a decreasing
+sequence of integers. Since the elements are sorted in some order, we should go for algorithm similar to
+binary search. The steps are as follows:
+1. Take two variable for storing start and end index. Variable start=0 and end=size-1
+2. Find the middle element of the array.
+3. See if the middle element is the maxima. If yes, return the middle element.
+4. Alternatively, If the middle element in increasing part, then we need to look for in mid+1 and end.
+5. Alternatively, if the middle element is in the decreasing part, then we need to look in the start and mid-
+1.
+6. Repeat step 2 to 5 until we get the maxima.
+ */
+
+function SearchBiotinicArrayMax(arr) {
+  let start = 0;
+  let end = arr.length - 1;
+  let maximaFound = 0;
+  let mid;
+
+  while (start <= end) {
+    mid = Math.floor((start + end) / 2);
+    if (arr[mid - 1] < arr[mid] && arr[mid] > arr[mid + 1]) {
+      //maxima
+      maximaFound = 1;
+      break;
+    } else if (arr[mid - 1] < arr[mid] && arr[mid + 1] > arr[mid]) {
+      //increasing
+      start = mid + 1;
+    } else if (arr[mid - 1] > arr[mid] && arr[mid + 1] < arr[mid]) {
+      //decreasing
+      end = mid - 1;
+    }
+  }
+  if (maximaFound == 0) {
+    return -1;
+  }
+
+  return arr[mid];
+}
+// const testData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 6, 5, 4, 3, 2, 1, 0];
+// console.log(SearchBiotinicArrayMax(testData));
+
+/**
+ * Search element in a bitonic array
+A bitonic array comprises of an increasing sequence of integers immediately followed by a decreasing
+sequence of integer s. To search an element in a bitonic array :
+1. Find the index or maximum element in the array. By finding the end of increasing part of the array, using
+modified binary search.
+2. Once we have the maximum element, search the given value in increasing part of the array using binary
+search.
+3. If the value is not found in increasing part, search the same value in decreasing part of the array using
+binary search.
+ */
+function SearchBiotinicArray(arr, target) {
+  let mid;
+  let start = 0;
+  let end = arr.length - 1;
+  while (start <= end) {
+    mid = Math.floor((start + end) / 2);
+
+    if (arr[mid] === target) {
+      return mid;
+    }
+    if (arr[mid] > target) {
+      start = mid + 1;
+    }
+    if (arr[mid] < target) {
+      end = mid - 1;
+    } else {
+      break;
+    }
+  }
+  return -1;
+}
+
+/**
+ * Occurrence counts in sorted Array
+Given a sorted array arr[] find the number of occurrences of a number.
+ */
+
+/**
+ * First approach: Brute force, Traverse the array and in linear time we will get the occurrence count of the
+number. This is done using one loop.
+The Time Complexity is O(n) and Space Complexity is O(1).
+ */
+// const testData = [1, 2, 3, 4, 5, 6, 7, 7, 7, 7, 7, 7, 7, 14.15, 16];
+console.time("brute force");
+function findKeyCount1(arr, key) {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === key) {
+      count++;
+    }
+  }
+  return count;
+}
+// console.log(findKeyCount1(testData, 7));
+console.timeEnd("brute force");
+
+console.time("optimised");
+function findKeyCount2(arr, key) {
+  // we will do binary search to find the element and then we will check for the first and last occurrence of the key using square root jump then start a linear search over the square root length
+
+  let firstIndex;
+  let lastIndex;
+  let firstFoundByBinarySearch;
+  let squareRoot = Math.floor(Math.sqrt(arr.length));
+
+  let start = 0;
+  let end = arr.length;
+  let mid;
+  while (start <= end) {
+    mid = start + Math.floor((end - start) / 2);
+    console.log(mid);
+    if (arr[mid] == key) {
+      console.log(mid, "mid", key, "key");
+      break;
+    }
+    if (arr[mid] > key) {
+      end = mid + 1;
+    }
+    if (arr[mid] < key) {
+      start = mid - 1;
+    } else {
+      return "not clear";
+    }
+  }
+
+  //  find first occurrence
+
+  //  substract square  root length from the midpoint
+  //  if value is less than zero start search from index 0
+  firstFoundByBinarySearch = mid;
+  mid = mid - squareRoot;
+  while (arr[mid] === key) {
+    mid = mid - squareRoot;
+    if (mid < 0) {
+      mid = 0;
+      break;
+    }
+  }
+  // //  for loop to walk linearly through m*(sqrt(n))
+  for (let i = mid; i <= firstFoundByBinarySearch; i++) {
+    if (arr[i] === key) {
+      firstIndex = i;
+      console.log("found first at ", firstIndex);
+      break;
+    }
+  }
+  // //  find last  occurrence
+
+  // //  add square  root length from the midpoint
+  // //  if value is greater than arr.length -1 end search at arr.lenght-1
+
+  mid = firstFoundByBinarySearch + squareRoot;
+  while (arr[mid] === key) {
+    mid = mid + squareRoot;
+    if (mid > arr.length - 1) {
+      mid = arr.length - 1;
+      break;
+    }
+  }
+  // //  for loop to walk linearly through m*(sqrt(n))
+  for (let j = firstFoundByBinarySearch; j <= mid; j++) {
+    if (arr[j] !== key) {
+      lastIndex = j;
+      console.log("found last at ", lastIndex - 1);
+      break;
+    }
+  }
+  return Math.abs(firstIndex - lastIndex);
+}
+// console.log(findKeyCount2(testData, 7));
+// console.timeEnd("optimised");
+
+/**
+ * Separate even and odd numbers in Array
+Given an array of even and odd numbers, write a program to separate even numbers from the odd
+numbers.
+ */
+/**
+ * First approach: allocate a separate array, then scan through the given array, and fill even numbers from the
+start and odd numbers from the end.
+ */
+function seperateEvenAndOdd1(arr) {
+  const separatedArr = [];
+
+  for (const num of arr) {
+    if (num % 2 == 0) {
+      separatedArr.unshift(num);
+    } else {
+      separatedArr.push(num);
+    }
+  }
+  return separatedArr;
+}
+const testData = [2, 1, 3, 6, 7, 8, 9, 10];
+
+console.log(seperateEvenAndOdd1(testData));
+
+/**
+ * Second approach: Algorithm is as follows.
+1. Initialize the two variable left and right. Variable left=0 and right= size-1.
+2. Keep increasing the left index until the element at that index is even.
+3. Keep decreasing the right index until the element at that index is odd.
+4. Swap the number at left and right index.
+ */
+function swap(a, b) {
+  let temp = a;
+  a = b;
+  b = temp;
+}
+
+function seperateEvenAndOdd2(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    if (arr[left] % 2 === 0) {
+      left++;
+    } else if (arr[right] % 2 === 1) {
+      right--;
+    } else {
+      [arr[left], arr[right]] = [arr[right], arr[left]];
+      left++;
+      right--;
+    }
+  }
+  return arr;
+}
+
+// console.log(seperateEvenAndOdd2(testData));
+
+/**
+ * Stock purchase-sell problem
+Given an array, whose nth element is the price of the stock on nth day. You are asked to buy once and sell
+once, on what date you will be buying and at what date you will be selling to get maximum profit.
+Or
+Given an array of numbers, you need to maximize the difference between two numbers, such that you can
+subtract the number, which appear before from the number that appear after it.
+ */
+
+/*
+First approach: Brute force, for each element in array find if there is some other element whose difference
+is maximum. This is done using two for loop, first loop to select, buy date index and the second loop to
+find its selling date entry.
+The Time Complexity is O(n2) and Space Complexity is O(1)Second approach: 
+*/
+
+/**
+ * Another clever solution is to keep track of the smallest value seen so far from the start.
+At each point, we can find the difference and keep track of the maximum profit. This is a linear solution.
+The Time Complexity of the algorithm is O(n) time. Space Complexity for creating count array is also
+O(1)
+ */
+function maxProfit(stock) {
+  let currMin = 0;
+  let maxProfit = 0;
+  let currProfit = 0;
+  let buy = 0;
+  let sell = 0;
+  let i;
+  for (i = 1; i <= arr.length - 1; i++) {
+    if (stock[i] < stock[currMin]) {
+      currMin = i;
+    }
+    currProfit = stock[i] - stock[currMin];
+    if (currProfit > maxProfit) {
+      maxProfit = currProfit;
+      buy = currMin;
+      sell = maxProfit;
+    }
+  }
+}
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,1 @@
-return
+first
