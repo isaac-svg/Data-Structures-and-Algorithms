ce3abb639a4e608b68d224bf23b76fc622bb3f1a
isaac-svg <isaacsakyi009@gmail.com>
2023-05-16 23:59:41 +0000

    new

--- leetcode/middle_of_linkedlist.cpp
+++ leetcode/middle_of_linkedlist.cpp
@@ -1,1 +1,21 @@
 // 876. Middle of the Linked List
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+ListNode *middleNode(ListNode *head)
+{
+    // fast and slow pointer approach
+    ListNode *slow = head;
+    ListNode *fast = head;
+    while (fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
