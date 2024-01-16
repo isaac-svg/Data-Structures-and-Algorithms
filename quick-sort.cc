d67ecfd7e817ec14a8c05872867364229e06cfbd
isaac-svg <isaacsakyi009@gmail.com>
2024-01-16 01:51:29 -0500

    insertion sort

---
+++ quick-sort.cc
@@ -0,0 +1,63 @@
+#include <iostream>
+
+
+
+/**
+ * Best Case O(n)
+ * Worst Case O(n^2)
+*/
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        std::cout << arr[i] << " ";
+    std::cout << std::endl;
+}
+
+void insertionSort(int arr[], int size){
+
+    for (size_t i = 1; i < size; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && key < arr[j])
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+
+        }
+
+        arr[j + 1] = key; 
+    }
+    
+}
+
+void insertionSort2(int arr[], int size){
+
+    for (size_t i = 1; i < size; i++)
+    {
+        int current = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && current < arr[j])
+        {
+            arr[j + 1] = arr[j];
+            --j;
+        }
+        arr[j+1] = current;
+    }
+    
+}
+int main(){
+
+    int arr[] = {12, 11, 13, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    std::cout << "Original array: ";
+    printArray(arr, n);
+
+    insertionSort2(arr, n);
+
+    std::cout << "Sorted array: ";
+    printArray(arr, n);
+    return (0);
+}
